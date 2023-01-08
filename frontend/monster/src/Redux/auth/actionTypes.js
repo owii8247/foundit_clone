87631290da216f91007341c0867cbcc15ab7be3b
Kundan Kumar Keshri <101567147+kundan799@users.noreply.github.com>
2023-01-08 20:35:21 +0530

    register

--- frontend/monster/src/Redux/auth/actionTypes.js
+++ frontend/monster/src/Redux/auth/actionTypes.js
@@ -5,3 +5,11 @@ export const AUTH_SUCCESS = "signin/success";
 export const AUTH_ERROR = "signin/error";
 
 export const AUTH_LOG_OUT = "signout";
+
+export const REGISTER_LODING="register/loding";
+
+export const REGISTER_SUCCESS="register/success";
+
+export const REGISTER_ERROR="register/error";
+
+
