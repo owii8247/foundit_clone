e82145a7a7b948ef93f6570734d5d42c9f1e9158
MD OWAIS ATHAR <owais.athar.8092@gmail.com>
2023-02-25 17:09:51 +0530

    Job Crud updated

--- backend/Models/Job.model.js
+++ backend/Models/Job.model.js
@@ -1,39 +1,32 @@
-const  mongoose = require("mongoose")
+const mongoose = require("mongoose")
 
-
-const jobModel =new mongoose.model("job", mongoose.Schema({
+const jobSchema = new mongoose.Schema({
     "id": { type: String, required: true },
-    "url": { type: String, required: true },
+    "image": { type: String, required: true },
     "job_title": { type: String, required: true },
+    "salary": { type: String, required: true },
     "category": { type: String, required: true },
     "company_name": { type: String, required: true },
-    "city": [
-        { type: String, required: true }
-    ],
-    "state": [
-        { type: String, required: true }
-    ],
-    "country": [
-        { type: String, required: true }
-    ],
-    "post_date": { type: String, required: true },
+    "city": { type: String, required: true },
+    "state": { type: String, required: true },
+    "country": { type: String, required: true },
+    "viewed": { type: String, required: true },
+    "applied": { type: String, required: true },
+    "experience": { type: String, required: true },
     "job_description": { type: String, required: true },
     "job_type": { type: String, required: true },
+    "industry": { type: String, required: true },
+    "functions": { type: String, required: true },
+    "roles": { type: String, required: true },
+    "skills": { type: String, required: true },
+    "education": { type: String, required: true },
+    "post_date": { type: String, required: true },
     "company_description": { type: String, required: true },
-    "job_post_lang": { type: String, required: true },
-    "html_job_description": { type: String, required: true },
-    "is_remote": { type: String, required: true },
-    "country_abbr": [
-        { type: String, required: true }
-    ],
-    "predicted_language": { type: String, required: true },
-    "inferred_iso3_lang_code": { type: String, required: true },
+    "is_remote": { type: String, required:true},
     "has_expired": { type: String, required: true },
-    "latest_expiry_check_date": { type: String, required: true },
-    "duplicate_status": { type: String, required: true },
-    "job_desc_char_count": { type: Number, required: true }
 })
-)
+
+const jobModel = mongoose.model("job", jobSchema)
 
 module.exports = {
     jobModel
--- backend/Routes/Job.route.js
+++ backend/Routes/Job.route.js
@@ -2,7 +2,7 @@ const { Router } = require("express")
 const { jobModel } = require("../Models/Job.model")
 const jobController = Router()
 
-
+// GET ALL JOBS
 jobController.get("/", async (req, res) => {
     console.log(req.query)
     const query = req.query
@@ -13,6 +13,7 @@ jobController.get("/", async (req, res) => {
 
 })
 
+// GET JOB BY ID
 jobController.get("/:id", async (req, res) => {
     const id = req.params.id
     try {
@@ -27,6 +28,37 @@ jobController.get("/:id", async (req, res) => {
 
 })
 
+// CREATE JOB 
+jobController.post("/create", async (req, res) => {
+    // const id = req.body
+    // console.log(id)
+    const { id,image, job_title,salary, category, 
+        company_name, city, state, country,
+        viewed, applied,  job_description,
+        job_type, industry, functions,
+        roles, skills, education,
+        post_date,experience,
+        is_remote, has_expired,
+        company_description
+    } = req.body
+
+    const new_job = new jobModel({
+        id,
+        image, job_title,salary, category, 
+        company_name, city, state, country,
+        viewed, applied,  job_description,
+        job_type, industry, functions,
+        roles, skills, education,
+        post_date,experience,
+        is_remote, has_expired,
+        company_description
+    })
+
+    await new_job.save()
+    res.send({ "Message": "Job Posted Successfully", new_job })
+})
+
+// DELETE JOB BY ID
 jobController.delete("/:id", async (req, res) => {
     const id = req.params.id;
     console.log(id)
@@ -38,5 +70,16 @@ jobController.delete("/:id", async (req, res) => {
         console.log(err)
     }
 })
+
+// PATCH JOB BY ID
+jobController.patch("/:id",async(req, res)=>{
+    try{
+      const  _id=req.params.id
+    const updatdata = await jobModel.findByIdAndUpdate(_id,req.body,{new:true});
+    res.send(updatdata)
+    }catch(err){
+      res.status(400).send(e.message);
+    }
+  });
 
 module.exports = { jobController }
